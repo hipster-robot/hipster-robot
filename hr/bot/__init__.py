4a4dd1cda587c486bf10ec54ef97a8c7f6605846
Hipster Robot <me@hipster-robot.com>
2017-03-27 23:27:04 +0200

    Fetch MongoDB database from the connection string

--- hr/bot/__init__.py
+++ hr/bot/__init__.py
@@ -1,14 +1,23 @@
 """Bot interfaces.
 """
-from .. import config
+from urllib.parse import urlparse
+
 from chatterbot import ChatBot
+
+from .. import config
+
+
+def mongodb_database(url, default):
+    db = urlparse(url).path or '/'
+    db = db.split('/')[1]
+    return db if db else default
 
 default_bot = ChatBot(
     'Default Bot',
     read_only=True,
     trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
     storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
-    database='chatterbot-english',
+    database=mongodb_database(config.MONGODB_URL, 'chatterbot-english'),
     database_uri=config.MONGODB_URL)
 
 default_bot.train('chatterbot.corpus.english')
