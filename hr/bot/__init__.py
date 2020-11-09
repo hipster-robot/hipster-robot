1c0eae8d56ebf8d859e884287d397f0cdb34bfb3
David Volquartz Lebech <david@lebech.info>
2020-11-09 21:45:37 +0100

    Remove MongoDB integration, update dependencies

--- hr/bot/__init__.py
+++ hr/bot/__init__.py
@@ -7,12 +7,6 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from .. import config
 
 
-def mongodb_database(url, default):
-    db = urlparse(url).path or "/"
-    db = db.split("/")[1]
-    return db if db else default
-
-
 default_bot = ChatBot(
     "Default Bot",
     read_only=True,
@@ -21,9 +15,8 @@ default_bot = ChatBot(
         {"import_path": "chatterbot.logic.BestMatch"},
         {"import_path": "hr.bot.logic.WolframAlpha"},
     ],
-    storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
-    database=mongodb_database(config.MONGODB_URL, "chatterbot-english"),
-    database_uri=config.MONGODB_URL,
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    database_uri=config.SQLITE_URL,
     wolfram_alpha_app_id=config.WOLFRAM_ALPHA_APP_ID,
 )
 
